d631ff5879090c3aa43dbfb2c19a46ea78c9412e
Bayu Satria Gemilang <bayusatria@Bayus-MacBook-Pro.local>
2020-05-09 08:12:19 +0700

    add static template

--- bayucv/urls.py
+++ bayucv/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+import info.views
 
 urlpatterns = [
+    path("", info.views.index, name="index"),
     path('admin/', admin.site.urls),
 ]
